e9550a9b11a7bff64297572e338bca28b279a066
Yang sujin <21500394@handong.edu>
2018-04-23 11:02:57 +0900

    make method

---
+++ src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
@@ -0,0 +1,22 @@
+package edu.handong.csee.java.example.inheritance;
+
+public class Faculty {
+	int facultyNumber;
+	
+    void reset(String newName, int newFacultyNumber) {
+    	
+    }
+    
+    int getFacultyNumber() {
+    	
+    }
+    
+    void setFacultyNumber(int newFacultyNumber) {
+    
+    }
+    
+    boolean equals(Faculty otherFaculty) {
+    	
+    }
+
+}
