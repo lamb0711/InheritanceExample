7c2c28c7dfe2a64fffe214a4963c1f65a622435f
Yang sujin <21500394@handong.edu>
2018-04-23 11:15:56 +0900

    add print method

--- src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
+++ src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
@@ -3,6 +3,16 @@ package edu.handong.csee.java.example.inheritance;
 public class Faculty extends Person{
 	int facultyNumber;
 	
+	public Faculty()    {
+		super();        
+		facultyNumber = 0;//Indicating no number yet    
+	}
+	
+	public Faculty(String initialName, int initialStudentNumber){
+		super(initialName);        
+		facultyNumber = initialStudentNumber;    
+	}
+	
     void reset(String newName, int newFacultyNumber) {
     	setName(newName);        
     	facultyNumber = newFacultyNumber;
@@ -15,6 +25,11 @@ public class Faculty extends Person{
     void setFacultyNumber(int newFacultyNumber) {
     	facultyNumber = newFacultyNumber;
     }
+    
+    public void writeOutput() {
+		System.out.println("Name: " + getName());
+		System.out.println("Student Number: " + facultyNumber); 
+	}
     
     boolean equals(Faculty otherFaculty) {
     	return this.hasSameName(otherFaculty) && (this.facultyNumber == otherFaculty.facultyNumber);
