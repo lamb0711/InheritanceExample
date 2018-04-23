76e5bd25de75007458d29ec9cc7e4990d8a861a8
Yang sujin <21500394@handong.edu>
2018-04-23 11:06:55 +0900

    add method

--- src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
+++ src/main/java/edu/handong/csee/java/example/inheritance/Faculty.java
@@ -4,19 +4,20 @@ public class Faculty {
 	int facultyNumber;
 	
     void reset(String newName, int newFacultyNumber) {
-    	
+    	setName(newName);        
+    	facultyNumber = newFacultyNumber;
     }
     
     int getFacultyNumber() {
-    	
+    	return facultyNumber;
     }
     
     void setFacultyNumber(int newFacultyNumber) {
-    
+    	facultyNumber = newFacultyNumber;
     }
     
     boolean equals(Faculty otherFaculty) {
-    	
+    	return this.hasSameName(otherFaculty) && (this.facultyNumber == otherFaculty.facultyNumber);
     }
 
 }
